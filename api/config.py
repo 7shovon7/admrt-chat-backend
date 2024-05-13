f9e5c4554b102162f35371e1ab69d6ed94a60715
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-13 07:59:02 +0600

    requirements added

--- api/config.py
+++ api/config.py
@@ -6,14 +6,14 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = f"SQLModel API - {os.getenv('ENV', 'development').capitalize()}"
-    DESCRIPTION: str = "A FastAPI + SQLModel production-ready API"
+    PROJECT_NAME: str = f"AdMrt Chat API - {os.getenv('ENV', 'development').capitalize()}"
+    DESCRIPTION: str = "Chat API for AdMrt.com"
     ENV: Literal["development", "staging", "production"] = "development"
     VERSION: str = "0.1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     DATABASE_URI: str = "sqlite:///database.db"
-    API_USERNAME: str = "svc_test"
-    API_PASSWORD: str = "superstrongpassword"
+    # API_USERNAME: str = "svc_test"
+    # API_PASSWORD: str = "superstrongpassword"
 
     class Config:
         case_sensitive = True
