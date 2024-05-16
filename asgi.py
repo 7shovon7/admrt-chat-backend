565afb2cf607397a658edf5bc72e2fbce6a12093
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-16 17:41:39 +0600

    procfile added

--- asgi.py
+++ asgi.py
@@ -4,11 +4,12 @@ import uvicorn
 from api.app import create_app
 from api.config import settings
 
+# TODO: remove it later
+old_db_path = 'database2.db'
+if os.path.exists(old_db_path):
+    os.remove(old_db_path)
+    
 api = create_app(settings)
 
 if __name__ == "__main__":
-    # TODO: remove it later
-    old_db_path = 'database2.db'
-    if os.path.exists(old_db_path):
-        os.remove(old_db_path)
     uvicorn.run("asgi:api", host="0.0.0.0", port=8080, reload=True)
